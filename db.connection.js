67eb7c07e504d362d17ef21ae9c0065f002a2aa3
ak3264114 <ak3264114@gmail.com>
2024-03-21 13:53:58 +0530

    database name removed

--- db.connection.js
+++ db.connection.js
@@ -4,7 +4,6 @@ const db = mysql.createConnection({
 	host: process.env.MYSQL_HOST_NAME,
 	user:process.env.MYSQL_USER ,
 	password: process.env.MYSQL_PASSWORD,
-	database: process.env.MYSQL_DATABASE,
 	port:  process.env.MYSQL_PORT,
 });
 
