0db489f16ef6d6e2995ce62f0bbf399149c1f90d
ak3264114 <ak3264114@gmail.com>
2024-03-21 13:18:47 +0530

    initial commit

--- db.connection.js
+++ db.connection.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-	host: "database-2.cdyacicia234.us-west-2.rds.amazonaws.com",
-	user: "admin",
-	password: "admin123",
-	database: "test",
-	port: "3306",
+	host: process.env.MYSQL_HOST_NAME,
+	user:process.env.MYSQL_USER ,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE,
+	port:  process.env.MYSQL_PORT,
 });
 
 db.connect((err) => {
